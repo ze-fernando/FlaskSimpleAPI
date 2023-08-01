94484679a13611007202ace916b4a76740281752
ze-fernando <jfenginner935@gmail.com>
2023-07-31 22:05:49 -0300

    Version without databse

---
+++ main/Animes.py
@@ -0,0 +1,39 @@
+animes = [
+    {
+        "id": 1,
+        "name": "Boku no hero",
+    },
+    {
+        "id": 2,
+        "name": "Bersek",
+    },
+    {
+        "id": 3,
+        "name": "Naruto",
+    },
+    {
+        "id": 4,
+        "name": "Attack on Titan",
+    },
+    {
+        "id": 5,
+        "name": "Fire Force",
+    },
+    {
+        "id": 6,
+        "name": "Hajime no Ippo",
+    },
+    {
+        "id": 7,
+        "name": "Baki Hanma",
+    },
+    {
+        "id": 8,
+        "name": "Ashita no joe",
+    },
+    {
+        "id": 9,
+        "name": "Bleach",
+    }
+
+]
--- main/main.py
+++ main/main.py
@@ -1,46 +1,9 @@
 from flask import Flask, jsonify, request
+from Animes import animes
 
 app = Flask(__name__)
 
-animes = [
-    {
-        "id": 1,
-        "name": "Boku no hero",
-    },
-    {
-        "id": 2,
-        "name": "Bersek",
-    },
-    {
-        "id": 3,
-        "name": "Naruto",
-    },
-    {
-        "id": 4,
-        "name": "Attack on Titan",
-    },
-    {
-        "id": 5,
-        "name": "Fire Force",
-    },
-    {
-        "id": 6,
-        "name": "Hajime no Ippo",
-    },
-    {
-        "id": 7,
-        "name": "Baki Hanma",
-    },
-    {
-        "id": 8,
-        "name": "Ashita no joe",
-    },
-    {
-        "id": 9,
-        "name": "Bleach",
-    }
 
-]
 
 
 @app.get("/animes/")

07f8aacb3e25d5da6e306b3823f97142ce372806
ze-fernando <jfenginner935@gmail.com>
2023-12-05 22:06:11 -0300

    finish routes

--- api/app.py
+++ api/app.py
@@ -1,46 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_pydantic_spec import FlaskPydanticSpec
-from src.services.services import *
+from src.routes import routes
 
 app = Flask(__name__)
+app.register_blueprint(routes.route)
 spec = FlaskPydanticSpec('flask', title='CRUD API')
 spec.register(app)
-
-
-@app.get("/animes")
-def getAll():
-    res = get()
-    
-    return jsonify(res)
-
-
-
-@app.get("/animes/<int:id>")
-def getOne(id:id): 
-   res = getById()
-   
-   return jsonify(res)
-
-
-@app.post("/animes")
-def createAnime():
-    res = create()
-
-    return jsonify(message="Successfully registered anime", dados=res)
-
-
-@app.delete("/animes/<int:id>")
-def deleteById(id:int):
-    res = delete(id)
-    
-    return jsonify(message="Successfully deleted anime", dados=res)
-
-
-@app.put("/animes/<int:id>")
-def editAnime(id:int):
-    body = request.json
-    res = put(id, body)
-    return jsonify(message="Successfully updated anime", dados=res)
 
 
 if __name__ == "__main__":
---
+++ api/src/routes/routes.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify, request, Blueprint
+from src.services.services import *
+
+route = Blueprint('routes', __name__)
+
+
+@route.get("/animes")
+def getAll():
+    res = get()
+    
+    return jsonify(res)
+
+
+
+@route.get("/animes/<int:id>")
+def getOne(id:id): 
+   res = getById()
+   
+   return jsonify(res)
+
+
+@route.post("/animes")
+def createAnime():
+    res = create()
+
+    return jsonify(message="Successfully registered anime", dados=res)
+
+
+@route.delete("/animes/<int:id>")
+def deleteById(id:int):
+    res = delete(id)
+    
+    return jsonify(message="Successfully deleted anime", dados=res)
+
+
+@route.put("/animes/<int:id>")
+def editAnime(id:int):
+    body = request.json
+    res = put(id, body)
+    return jsonify(message="Successfully updated anime", dados=res)
